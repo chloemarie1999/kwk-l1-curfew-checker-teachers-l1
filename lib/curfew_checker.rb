24a136a4b9bf0e160f6fbf66635fe7d9b51fc8db
Chloé P <chloepuck13@yahoo.com>
2018-07-24 18:56:53 +0000

    Done.

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -3,15 +3,25 @@ def simple_curfew_checker(time)
 end
 
 def curfew_checker(time)
-  # code goes here
+  if time== 11
+    "You're just on time."
+  elsif time>11
+    "You're late! You're going to get in trouble."
+  else 
+    nil
 end
 
 def complex_curfew_checker(time)
-  # code goes here
+  if time== 11
+    "You're just on time."
+  elsif time>11
+    "You're late! You'll be in trouble."
+  else 
+    "Keep having fun."
 end
 
 def deluxe_curfew_checker(time)
-  # code goes here
+  if cu
 end
 
 def platinum_curfew_checker(current_time, curfew_time)
