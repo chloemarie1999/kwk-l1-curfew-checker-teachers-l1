061b4b1b821a47075f334cdd87a44ded5f4ec401
Chloé P <chloepuck13@yahoo.com>
2018-07-24 18:49:41 +0000

    automatically backed up by learn

--- curfew_checker.rb
+++ curfew_checker.rb
@@ -1,2 +1,12 @@
 def time(x)
-  if x== 
+  if x=11
+    puts "You made it just in time!"
+  elsif x>11
+    puts "You're late. You're going to be in trouble."
+  else
+    puts "You still have time until your curfew."
+  end
+end
+
+time(11)
+puts time
